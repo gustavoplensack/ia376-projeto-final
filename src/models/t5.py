18db1136391f5a4cee41ec12b3bc0d4dbe8feb97
Gustavo <gustavoplensack@gmail.com>
2021-01-06 22:41:27 -0300

    Add train script

---
+++ src/models/t5.py
@@ -0,0 +1,142 @@
+'''
+Implements PL module for T5
+'''
+from random import choice
+
+import pytorch_lightning as pl
+from decouple import config
+from numpy import average
+from torch import optim, stack
+from torch.utils.data import DataLoader
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+from src.metrics import compute_exact_match, compute_f1
+
+# Experiment configuration
+T5_TYPE = config('T5_TYPE', default='t5-small', cast=str)
+SEQ_LEN = config('SEQ_LEN', default=128, cast=int)
+LEARNING_RATE = config('LEARNING_RATE', default=3e-4, cast=float)
+
+# Get a tokenizer instance
+T5_TOK = T5Tokenizer.from_pretrained(T5_TYPE)
+
+
+class T5Module(pl.LightningModule):
+    '''
+    Neural network built with an efficient-net for image feature extraction and
+    a T5 for text generation.
+    '''
+
+    def __init__(self, train_dataloader: DataLoader,
+                 val_dataloader: DataLoader,
+                 test_dataloader: DataLoader):
+        super().__init__()
+
+        self.t5 = \
+            T5ForConditionalGeneration.from_pretrained(T5_TYPE,
+                                                       return_dict=True,
+                                                       output_attentions=True)
+
+        self._train_dataloader = train_dataloader
+        self._val_dataloader = val_dataloader
+        self._test_dataloader = test_dataloader
+
+    def forward(self, x_tokens, x_mask, x_original,
+                y_tokens, y_mask, y_original):
+
+        if self.training:
+            loss = self.t5.forward(
+                input_ids=x_tokens,
+                labels=y_tokens,
+            )[0]
+
+            return loss
+        else:
+            predicted_token_ids = self.t5.generate(
+                x_tokens, max_length=SEQ_LEN
+            )
+
+            return predicted_token_ids
+
+    def training_step(self, batch, batch_idx):
+
+        x_tokens, x_mask, x_original, y_tokens, y_mask, y_original = batch
+        loss = self(x_tokens, x_mask, x_original,
+                    y_tokens, y_mask, y_original)
+
+        return {"loss": loss, "log": {"step_loss": loss}}
+
+    def training_epoch_end(self, outputs):
+        loss = stack([x['loss'] for x in outputs]).mean()
+
+        tqdm_dict = {"train_loss": loss}
+
+        return {"log": tqdm_dict, "progress_bar": tqdm_dict}
+
+    def validation_step(self, batch, batch_idx):
+        x_tokens, x_mask, x_original, y_tokens, y_mask, y_original = batch
+
+        preds = self(x_tokens, x_mask, x_original,
+                     y_tokens, y_mask, y_original)
+
+        decoded_preds = [T5_TOK.decode(
+            pred, skip_special_tokens=True) for pred in preds]
+
+        return {"pred": decoded_preds, "target": y_original}
+
+    def validation_epoch_end(self, outputs):
+        trues = sum([list(x['target']) for x in outputs], [])
+        preds = sum([list(x['pred']) for x in outputs], [])
+
+        # Select a random sample from the trues and preds
+        true, pred = choice(list(zip(trues, preds)))
+
+        print(f"\n tgt: {true}\n prd: {pred}\n")
+
+        em = average([compute_exact_match(g, r) for g, r in zip(preds, trues)])
+        f1 = average([compute_f1(g, r) for g, r in zip(preds, trues)])
+
+        self.log("val_em", em, prog_bar=True)
+        self.log("val_f1", f1, prog_bar=True)
+
+        return
+
+    def test_step(self, batch, batch_idx):
+        x_tokens, x_mask, x_original, y_tokens, y_mask, y_original = batch
+
+        preds = self(x_tokens, x_mask, x_original,
+                     y_tokens, y_mask, y_original)
+
+        decoded_preds = [T5_TOK.decode(
+            pred, skip_special_tokens=True) for pred in preds]
+
+        return {"pred": decoded_preds, "target": y_original}
+
+    def test_epoch_end(self, outputs):
+        trues = sum([list(x['target']) for x in outputs], [])
+        preds = sum([list(x['pred']) for x in outputs], [])
+
+        # Select a random sample from the trues and preds
+        true, pred = choice(list(zip(trues, preds)))
+
+        print(f"\n tgt: {true}\n prd: {pred}\n")
+
+        em = average([compute_exact_match(g, r) for g, r in zip(preds, trues)])
+        f1 = average([compute_f1(g, r) for g, r in zip(preds, trues)])
+
+        self.log("test_em", em, prog_bar=True)
+        self.log("test_f1", f1, prog_bar=True)
+
+        return
+
+    def train_dataloader(self):
+        return self._train_dataloader
+
+    def val_dataloader(self):
+        return self._val_dataloader
+
+    def test_dataloader(self):
+        return self._test_dataloader
+
+    def configure_optimizers(self):
+        return optim.AdamW(self.parameters(), lr=LEARNING_RATE)
--- src/scripts/train_model.py
+++ src/scripts/train_model.py
@@ -1,18 +1,111 @@
 '''
 This file implements the training script
 '''
+import os
 from pathlib import Path
+from typing import Tuple
+
+import pytorch_lightning as pl
+import torch
+from decouple import config
+from pytorch_lightning import seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint
+from torch.utils.data import DataLoader
 
 from src.data import OCRDataset
+from src.models import T5Module
+
+# Get envvars
+BATCH_SIZE = config('BATCH_SIZE', default=8, cast=int)
+BATCH_SIZE = config('BATCH_SIZE', default=8, cast=int)
 
 # Get project root
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 
+# Get data files
+TRAIN_FILES = f'{PROJECT_ROOT}/data/train'
+TEST_FILES = f'{PROJECT_ROOT}/data/test'
+GCV_OCR_CSV = f'{PROJECT_ROOT}/data/ocr_baseline.csv'
 
-train_files = f'{PROJECT_ROOT}/data/train'
-train_csv_path = f'{PROJECT_ROOT}/data/ocr_baseline.csv'
 
-dataset = OCRDataset(train_files, train_csv_path)
+def _make_datasets() -> Tuple[OCRDataset, OCRDataset]:
+    '''
+    Instantiate train and test datasets train and test data.
+
+    Returns:
+
+        - A tuple with train and test datasets.
+    '''
+
+    train_dataset = OCRDataset(TRAIN_FILES, GCV_OCR_CSV)
+    test_dataset = OCRDataset(TEST_FILES, GCV_OCR_CSV)
+
+    return train_dataset, test_dataset
 
 
-print(dataset[0])
+def _make_dataloader(dataset: OCRDataset, is_train: bool = True) -> DataLoader:
+    '''
+    Instantiantes a dataloader class for a given dataset.
+    NOTE: this class is just a wrapper for default torch implemetation.
+
+    Args:
+
+        - dataset instance of OCRDataset
+        - is_train: parameter to indicate if it is a train instance, if is
+            makes it shuffle the data.
+
+    Returns:
+
+        - A dataloader instance for the specified dataset.
+    '''
+
+    batch_size = BATCH_SIZE if is_train else 1
+
+    return DataLoader(dataset, batch_size=batch_size, shuffle=is_train)
+
+
+def main():
+    '''
+    Instantiates datasets, dataloaders and model.
+
+    Then it starts training!
+    '''
+    seed_everything(42)
+
+    print('Instantiating datasets ...')
+    train_dataset, test_dataset = _make_datasets()
+
+    train_dataloader = _make_dataloader(train_dataset, is_train=True)
+    test_dataloader = _make_dataloader(test_dataset, is_train=False)
+
+    model = T5Module(train_dataloader, test_dataloader, test_dataloader)
+
+    checkpoint_path = './epoch=49.ckpt'
+    checkpoint_dir = os.path.dirname(os.path.abspath(checkpoint_path))
+    print(f'Files in {checkpoint_dir}: {os.listdir(checkpoint_dir)}')
+    print(f'Saving checkpoints to {checkpoint_dir}')
+    checkpoint_callback = ModelCheckpoint(filepath=checkpoint_dir,
+                                          # Keeps all checkpoints.
+                                          save_top_k=-1,
+                                          monitor='val_f1')
+
+    resume_from_checkpoint = None
+    if os.path.exists(checkpoint_path):
+        print(f'Restoring checkpoint: {checkpoint_path}')
+        resume_from_checkpoint = checkpoint_path
+        print('Using checkpoint:', resume_from_checkpoint)
+
+    trainer = pl.Trainer(gpus=1,
+                         max_epochs=1,
+                         check_val_every_n_epoch=1,
+                         profiler=True,
+                         checkpoint_callback=checkpoint_callback,
+                         progress_bar_refresh_rate=50,
+                         resume_from_checkpoint=resume_from_checkpoint)
+
+    trainer.fit(model)
+    return
+
+
+# Calls the train script main function
+main()
