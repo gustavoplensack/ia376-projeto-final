27d29bbd5bf3a09a8f8d815502f06531bf63f4e8
Gustavo <gustavoplensack@gmail.com>
2021-01-09 21:18:04 -0300

    Add experiment dataset switch

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,2 +1,15 @@
-from .dataset import OCRDataset
+from decouple import config
+
+from .baseline_dataset import BaselineDataset
 from .utils import load_text_jpeg_pairs
+
+EXPERIMENT_DATASET_KEY = config(
+    'EXPERIMENT_DATASET_KEY', default='baseline', cast=str)
+
+
+DATASETS_DICT = {
+    'baseline': BaselineDataset,
+}
+
+# OCR dataset is the class that is meant to be used to
+OCRDataset = DATASETS_DICT[EXPERIMENT_DATASET_KEY]
