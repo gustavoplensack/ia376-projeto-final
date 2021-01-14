4b013f27fdaeebe361f792669a51e6dabc04c846
gustavoplensack <gustavoplensack@gmail.com>
2021-01-13 22:58:11 -0300

    Add all key experiment (#20)

--- src/data/scripts/make_single_key_dataset.py
+++ src/data/scripts/make_single_key_dataset.py
@@ -5,7 +5,7 @@ from src.data import multiply_dataset_samples
 # Get project root
 PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
 
-KEY_POOL = ['address', 'company']
+KEY_POOL = ['address', 'company', 'value', 'date']
 
 multiply_dataset_samples(f'{PROJECT_ROOT}/data/train/', KEY_POOL)
 multiply_dataset_samples(f'{PROJECT_ROOT}/data/test/', KEY_POOL)
